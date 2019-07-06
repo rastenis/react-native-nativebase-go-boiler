66fbe4c254403f3cb1afddccf75ceb4ab476adb2
Scharkee <matas2k@gmail.com>
2019-07-06 14:16:27 +0300

    env and secure sessions (non persistant), json response object definitions

--- src/server/index.go
+++ src/server/index.go
@@ -1,38 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 )
 
-var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+// Person : stores a single person's data
+type Person struct {
+	Name   string
+	Number int
+}
+
+// SessionData : to send back session data
+type SessionData struct {
+	People []Person
+	Auth   bool
+}
+
+var store = sessions.NewCookieStore(securecookie.GenerateRandomKey(16),
+	securecookie.GenerateRandomKey(16))
+
+func init() {
+	store.Options = &sessions.Options{
+		MaxAge:   3600 * 8, // 8 hours
+		HttpOnly: true,
+	}
+}
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	log.Print(securecookie.GenerateRandomKey(16),
+		securecookie.GenerateRandomKey(16))
+
 	router := mux.NewRouter()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		// gettinng the session
-		session, _ := store.Get(r, "boiler-session")
+		session, _ := store.Get(req, "boiler-session")
 
 		session.Values["testing"] = "session storage"
-		session.Save(r, w)
-
-		fmt.Fprint(w, "This is the index page.")
-	})
-
-	router.HandleFunc("/session", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Refreshing session.")
-		fmt.Fprint(w, "Session.")
-		session, _ := store.Get(r, "boiler-session")
-		for key, val := range session.Values {
-			fmt.Printf("%s=%s\n", key, val)
+		// Save.
+		if err = sessions.Save(req, res); err != nil {
+			log.Printf("Error saving session: %v", err)
 		}
 
-		// send back the session
+		fmt.Fprint(res, "This is the index page.")
 	})
 
-	http.ListenAndServe(":80", router)
+	router.HandleFunc("/session", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Println("Retreiving session...")
+		session, _ := store.Get(req, "boiler-session")
+		log.Print(session.Values["testing"])
+
+		// send back the session + static data
+		sessionData := SessionData{[]Person{{"Jack Hill", 421}, {"Jack Wright", 212}}, true}
+		js, err := json.Marshal(sessionData)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		res.Header().Set("Content-Type", "application/json")
+		res.Write(js)
+	})
+
+	http.ListenAndServe(":8080", router)
 }
